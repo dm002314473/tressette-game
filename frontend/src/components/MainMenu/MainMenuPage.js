8c6de3e169aaf27435f7e254aa3f766d0a89682d
DominkoMilic <dmilic04@fesb.hr>
2024-12-18 22:11:14 +0100

    main menu page + *.css to green shades

--- frontend/src/components/MainMenu/MainMenuPage.js
+++ frontend/src/components/MainMenu/MainMenuPage.js
@@ -3,9 +3,34 @@ import { useNavigate } from "react-router-dom";
 import "./MainMenuPage.css";
 
 function MainMenuPage() {
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>upada main menu page</h1>
+    <div className="menu-container">
+      <button className="exit-button" onClick={handleExit}>
+        Izlaz
+      </button>
+      <div className="menu-content">
+        <h1 className="menu-title">Trešeta</h1>
+        <ul className="menu-options">
+          <li>
+            <button className="menu-button">Stvori igru</button>
+          </li>
+          <li>
+            <button className="menu-button">Pridruži se igri</button>
+          </li>
+          <li>
+            <button className="menu-button">Pravila i upute</button>
+          </li>
+          <li>
+            <button className="menu-button">Statistike</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
